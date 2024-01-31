735e752b293b6ea1ddca110b3b1a67262d4cdf86
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-01-31 03:53:59 +0000

    Data types in javascript

---
+++ Basics/01variables.js
@@ -0,0 +1,11 @@
+
+const accountId=144553
+let accountEmail="sanjaypawar@gmail.com"
+var accountPassword="12334"
+acccountCity="Bhopal"
+
+// accountId=2  not allowed
+
+console.log(accountId);
+console.log(typeof(acccountCity));
+console.table([accountId,accountEmail,accountPassword,acccountCity]);
---
+++ Basics/02datatypes.js
@@ -0,0 +1,12 @@
+"use Strict";  //treat all js code as newer version
+
+// alert("hello alert") 
+
+let name="sanjay"
+
+let age=20
+
+let isLoggedIn=false
+
+// Number,bigint,boolean,null,undefined,Symbol
+
