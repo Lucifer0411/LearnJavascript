d6c1613d529d06ebfd647c78220e64efb82cf6b8
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-01-31 13:02:08 +0000

    Data type summary

--- Basics/02datatypes.js
+++ Basics/02datatypes.js
@@ -1,6 +1,9 @@
 "use Strict";  //treat all js code as newer version
 
-// alert("hello alert") 
+
+//Primitive Datatypes
+
+//7 types :String, Number, Boolean, null, undefined, Symbol, BigInt
 
 let name="sanjay"
 
@@ -8,5 +11,48 @@ let age=20
 
 let isLoggedIn=false
 
-// Number,bigint,boolean,null,undefined,Symbol
+const score = 100
+const scoreValue = 100.0
+const outsideTemp=null
+
+let userEmail;
+
+const id=Symbol("123")
+const anotherId=Symbol("123")
+console.log(id);
+console.log(anotherId);
+console.log(anotherId==id);//false
+
+const bigNumber=3242352533535234n//BigInt
+
+//Reference (Non primitive)
+
+//Array, Objects, Functions
+
+
+//Array
+const heros=['shaktiman','naagrajun','doga'];
+
+
+//Object
+const myInfo={
+    name:'sanjay',
+    age:20,
+
+}
+
+
+//Function
+const myFunction=function (){
+    console.log("hello world");
+}
+
+console.log(typeof(myFunction));//function
+console.log(typeof(myInfo));//object
+console.log(typeof(heros));//object
+
+
+
+
+
 
