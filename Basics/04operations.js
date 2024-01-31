ffb7014b6dc46424f110ca13a28e57bc3e502709
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-01-31 12:44:56 +0000

    Operation and Datatype comparison

---
+++ Basics/04operations.js
@@ -0,0 +1,41 @@
+let value=3
+
+let negValue=-value
+
+console.log(negValue);
+
+// console.log(3+3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(3**3);
+
+let str1="hello "
+let str2="sanjay"
+
+let str3=str1+str2
+console.log(str3);
+
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+
+console.log("1"+2+2);//122
+
+console.log(1+2+"3");//33
+
+
+console.log(true+1);
+// console.log(true++); error
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+console.log(num1,num2,num3) //4 4 4 
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter); //101
+
+
+
