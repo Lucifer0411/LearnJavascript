130701239934171324d69f6a83e594538da101a1
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-01-31 12:16:48 +0000

    data type conversion in js

---
+++ Basics/03datatypeconversion.js
@@ -0,0 +1,35 @@
+let score="33"
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+
+
+//Number conversion
+//"33"=>33
+//"33abc"=>NaN
+//true=>1: false=>0
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//Boolean Conversion
+//1=>true
+//0=>false
+//""=>false
+//"sanjay"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(typeof(stringNumber));
+
+
+
+
+
+
