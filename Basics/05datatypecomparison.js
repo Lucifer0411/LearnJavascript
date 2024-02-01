af3deec50e245b7ba06c80dc29b856ccd5aa8216
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-02-01 03:48:30 +0000

    memory store in js

--- Basics/05datatypecomparison.js
+++ Basics/05datatypecomparison.js
@@ -21,4 +21,27 @@ console.log(undefined<=0)//false
 //=== 
 
 console.log("2"===2); //false
+//-------------------------------------------------------------------------------
+//Memory type in javascript
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+let myClgName="sagar institute of research and technology"
+
+let anotherName=myClgName
+
+anotherName="chaiaurcode"
+console.log(anotherName);
+
+let user={
+    email:"user@google.com",
+    upi:"user@ybi"
+}
+
+let user2=user
+user2.email="sanjay@google.com"
+
+console.log(user.email)
+console.log(user2.email)
+
 
