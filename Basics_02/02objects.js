780f2204c6239485a1d3b46703fe285431c227ed
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-02-03 17:50:15 +0000

    Object part 2

---
+++ Basics_02/02objects.js
@@ -0,0 +1,59 @@
+//Singleton Objects
+
+const tinderUser=new Object()
+
+tinderUser.id="123abc"
+tinderUser.name="Naman"
+tinderUser.isLoggedIn=false
+// console.log(tinderUser)
+
+const regularUser={
+    email:"sanjay@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"sanjay",
+            lastname:"pawar"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.lastname);
+// console.log(regularUser.fullname.userfullname);
+
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+// const obj3=Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+
+const users=[
+    {
+    id:1,
+    email:"sanjaypawar@gmail.com"
+},
+    {
+    id:2,
+    email:"mayank@gmail.com"
+},
+    {
+    id:3,
+    email:"naman@gmail.com"
+},
+]
+console.log(users[1]);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+console.log(tinderUser.hasOwnProperty("isLogged"));
+
+
+
+
