5670c024a175bd11f33fa845706c5fcb8ad7d1e7
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-02-07 17:31:01 +0000

    scopes in js

--- Basics_03/02_scopes.js
+++ Basics_03/02_scopes.js
@@ -7,10 +7,40 @@ if(true){
     let a = 10
     // const b = 20
     var c = 30
-    console.log(a);
+    // console.log(a);
 
 }
 c=3000
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+function one(){
+    const username="sanjay"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+one()
+
+if(true){
+    const username="sanjay"
+    if(username==="sanjay"){
+        const website=" youtube"
+        // console.log(username+website);
+    }
+}
+
+function addone(num){
+    return num+1;
+}
+console.log(addone(5));
+
+const addTwo=function(num){
+    return num+2
+}
+console.log(addTwo(5));
