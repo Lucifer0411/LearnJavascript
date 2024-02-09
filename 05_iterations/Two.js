6cdb2d91864eb3bf38ae2e5d37809d5ec4497f11
Lucifer0411 <95531566+Lucifer0411@users.noreply.github.com>
2024-02-09 16:14:30 +0000

    while loop in js

---
+++ 05_iterations/Two.js
@@ -0,0 +1,23 @@
+//while
+let index=0
+while(index<=10){
+    console.log("value of index is "+index);
+    index+=2
+}
+
+let myArray=['flash','batman','superman']
+
+let i=0
+while(i<myArray.length){
+    console.log(myArray[i]);
+    i++
+}
+
+
+//do while
+
+let score=11
+do{
+    console.log(`score is: ${score}`);
+    score++;
+}while(score<=10)
